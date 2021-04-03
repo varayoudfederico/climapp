8b27864b5980bf259828501adc14ad19f259302b
Fede <varayoudfederico@gmail.com>
2021-04-03 17:28:11 -0300

    Correcciones y mejoras

--- backend/src/controllers/locationController.js
+++ backend/src/controllers/locationController.js
@@ -1,14 +1,29 @@
 const axios = require("axios");
 
-const fetchLocation = () => {
+const fetchCurrentLocation = () => {
 	const url = `http://ip-api.com/json/?fields=status,message,countryCode,city,lat,lon`;
+	return axios.get(url);
+};
+
+const fetchLocationByIP = (ip) => {
+	const url = `http://ip-api.com/json/24.48.0.1?fields=status,message,countryCode,city,lat,lon`;
 	return axios.get(url);
 };
 
 const getLocation = async (req, res) => {
 	try {
-		let response = await fetchLocation();
-		res.status(200).json({ status: "success", data: response.data });
+		console.log(req.ip);
+		if (req.ip === "::1") {
+			console.log("es local");
+			let response = await fetchCurrentLocation();
+			res.status(200).json({ status: "success", data: response.data });
+		} else {
+			console.log("no es local");
+			let response = await fetchLocationByIP(req.ip);
+			res.status(200).json({ status: "success", data: response.data });
+		}
+
+		// res.status(200).json({ status: "success", data: response.data });
 	} catch (e) {
 		res
 			.status(404)
@@ -18,5 +33,6 @@ const getLocation = async (req, res) => {
 
 module.exports = {
 	getLocation,
-	fetchLocation,
+	fetchCurrentLocation,
+	fetchLocationByIP,
 };

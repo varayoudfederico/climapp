4b5bdfc7a70f23109c9aeec60c3ce91f56d41460
Fede <varayoudfederico@gmail.com>
2021-04-03 18:07:29 -0300

    Added heroku

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const hostUrl = "http://localhost:5000/v1";
-const hostUrl = "https://cryptic-springs-45012.herokuapp.com/v1";
+const hostUrl = "https://climappv1.herokuapp.com/v1";
 
 export const fetchLocation = () => {
 	const url = hostUrl + `/location`;
@@ -14,11 +14,13 @@ export const fetchCity = (ciudad) => {
 };
 
 export const fetchWeatherByCity = (ciudad) => {
-	const url = hostUrl + `/current/coordinates?&lat=${ciudad.lat}&lon=${ciudad.lon}`;
+	const url =
+		hostUrl + `/current/coordinates?&lat=${ciudad.lat}&lon=${ciudad.lon}`;
 	return axios.get(url);
 };
 
 export const fetchForecastByCity = (ciudad) => {
-	const url = hostUrl + `/forecast/coordinates?&lat=${ciudad.lat}&lon=${ciudad.lon}`;
+	const url =
+		hostUrl + `/forecast/coordinates?&lat=${ciudad.lat}&lon=${ciudad.lon}`;
 	return axios.get(url);
 };

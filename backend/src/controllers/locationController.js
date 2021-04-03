54ec9667a0906fe2eecec634c7d42ab2de3513c3
Fede <varayoudfederico@gmail.com>
2021-04-03 18:24:09 -0300

    Correcciones

--- backend/src/controllers/locationController.js
+++ backend/src/controllers/locationController.js
@@ -5,6 +5,26 @@ const fetchCurrentLocation = () => {
 	return axios.get(url);
 };
 
+const getClientIp = (req) => {
+	var ipAddress;
+	// Amazon EC2 / Heroku workaround to get real client IP
+	var forwardedIpsStr = req.header("x-forwarded-for");
+	if (forwardedIpsStr) {
+		// 'x-forwarded-for' header may return multiple IP addresses in
+		// the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+		// the first one
+		var forwardedIps = forwardedIpsStr.split(",");
+		ipAddress = forwardedIps[0];
+	}
+	if (!ipAddress) {
+		// Ensure getting client IP address still works in
+		// development environment
+		ipAddress = req.connection.remoteAddress;
+	}
+	console.log("IP DETECTADA: ", ipAddress);
+	return ipAddress;
+};
+
 const fetchLocationByIP = (ip) => {
 	const url = `http://ip-api.com/json/${ip}?fields=status,message,countryCode,city,lat,lon`;
 	return axios.get(url);
@@ -19,7 +39,7 @@ const getLocation = async (req, res) => {
 			res.status(200).json({ status: "success", data: response.data });
 		} else {
 			console.log("no es local: ", req.ip);
-			let response = await fetchLocationByIP(req.ip);
+			let response = await fetchLocationByIP(getClientIp(req));
 			res.status(200).json({ status: "success", data: response.data });
 		}
 
